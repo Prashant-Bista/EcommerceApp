c16f229677e0d9364f97b0f459d5da20ebf71b0b
Prashant <bistaprashant1@gmail.com>
2025-05-06 22:10:50 +0545

    added exception handling service

--- lib/services/exceptionService/exceptionService.dart
+++ lib/services/exceptionService/exceptionService.dart
@@ -1,19 +1,47 @@
 import 'package:dio/dio.dart';
 import 'package:ecommerce_app/services/http_service/dio_service.dart';
+import 'package:flutter/foundation.dart';
 
 class ExceptionService {
-  static handleException({required Function() tryFunc, required Function() finallyFunc}){
-    try{
+
+  static handleException({
+    required void Function() tryFunc,
+    Future<void> Function(Object?)? onDioException,
+    Future<void> Function(Object?)? onGeneralException,
+    required void Function() finallyFunc,
+    bool showAlertMessage=true
+  }) {
+    try {
       tryFunc();
-    }
-    on DioException catch (e){
-      throw Exception("DioErorr");
-    }
-    catch(e){
-      return Exception("Some error");
-    }
-    finally{
+    } on DioException catch (e) {
+      if(e.message!.contains("violates not-null constraint")){
+        //BotToast.showText(text: "Something went wrong");
+        return;
+      }
+      if(onDioException!=null){
+        onDioException(e);
+        return;
+      }
+      //botToast.showText(text:"${e.message}"
+      if (showAlertMessage){
+        //BotToast.showText(text:"${e.message}");
+        return;
+      }
+      
+    } catch (e) {
+      if(onGeneralException!=null){
+        onGeneralException(e);
+        return;
+      }
+      if(kDebugMode){
+        print("Dio Exception Here $e");
+      }
+      if (showAlertMessage){
+        //BotToast.showText(text:"${e.message}");
+        return;
+      }
+    } finally {
       finallyFunc();
-  }
+    }
   }
 }
